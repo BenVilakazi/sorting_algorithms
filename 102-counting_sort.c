cba9d3c63567ad7800a934f7af2facf1c533ded9
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-12-16 00:19:05 +0200

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,86 +1,101 @@
-#include "sort.h"
 #include <stdio.h>
-/**
- * _bigest -  Give me the largest number in a array of integers
- * @array: The Int array
- * @size: Size of array
- * Return: The largest number
- */
-int _bigest(int *array, size_t size)
-{
-	size_t i;
-	int k = 0;
+#include <stdlib.h>
+#include "sort.h"
 
-	for (i = 0; i < size; i++)
+int recurrences(int *array, size_t size, int range);
+
+/**
+ * recurrences - Count number of time value is in list
+ * @array: Array to be sorted
+ * @size: Size of the array
+ * @range: Distance between largest number
+ * Return: Void
+ */
+
+int recurrences(int *array, size_t size, int range)
+{
+	int total = 0;
+	size_t count;
+
+	for (count = 0; count < size; count++)
 	{
-		if (k < array[i])
-			k = array[i];
+		if (array[count] == range)
+		{
+			total++;
+		}
 	}
-
-	return (k);
+	return (total);
 }
+
+
 /**
- * _memset -  Create a integer array and set each space in 0
- * @size: Size of array
- * Return: The integer array
+ * counting_sort - Sorts array in ascending order
+ * @array: Array to be sorted
+ * @size: Size of the array
+ * Return: Void
  */
-int *_memset(int size)
-{
-	int *ptr = NULL;
-	int i;
 
-	ptr = malloc((size) * sizeof(int));
-
-	for (i = 0; i < size; i++)
-		ptr[i] = 0;
-
-	return (ptr);
-}
-/**
- * counting_sort - sort an array with the counting sort algorithm
- * @array: The Int array
- * @size: Size of array
- * Return: The sorted list
- */
 void counting_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int k = 0;
-	int *ptr = NULL, *sort_ar = NULL;
+	size_t count1, count2;
+	int largest_num = 0, range = 0, temp = 0;
+	int *count_array, *sorted_array;
 
-	if (size < 2)
-		return;
-	/*Know the largest number in the array*/
-	k = _bigest(array, size);
-	/*Make the help array*/
-	ptr = _memset(k + 1);
-	if (!ptr)
-		return;
-	/*Set the values for sorting*/
-	for (i = 0; i < size; i++)
-		for (j = 0; (int)j < k + 1; j++)
-			if ((int)j == array[i])
-				ptr[j] += 1;
-	/*Modificate the count in the array*/
-	for (i = 0; (int)i < k; i++)
-		ptr[i + 1] = ptr[i] + ptr[i + 1];
-	print_array(ptr, k + 1);
-	/*Create the sort array*/
-	sort_ar = malloc(size * sizeof(int));
-	if (!sort_ar)
+	if (!array || size < 2)
 	{
-		free(ptr);
 		return;
 	}
 
-	for (i = 0; i < size; i++)
+	/* Find the largest number in list */
+	for (count1 = 0; count1 < size; count1++)
 	{
-		sort_ar[ptr[array[i]] - 1] = array[i];
-		ptr[array[i]] -= 1;
+		if (array[count1] > largest_num)
+		{
+			largest_num = array[count1];
+		}
 	}
-	for (j = 0; j < size; j++)
-		array[j] = sort_ar[j];
+	/* Init. count_array as size of largest number (range) */
+	count_array = malloc(sizeof(int) * (largest_num + 1));
 
-	free(sort_ar);
-	free(ptr);
+	if (!count_array)
+	{
+		return;
+	}
+
+	/* Count recurrence number within range of largest number*/
+	for (count2 = 0; count2 < ((size_t)largest_num + 1); count2++)
+	{
+		if (count2 == 0)
+		{
+			count_array[count2] = recurrences(array, size, range);
+		}
+		else
+		{
+			temp = count_array[count2 - 1] + recurrences(array, size, range);
+			count_array[count2] = temp;
+		}
+		range++;
+	}
+	print_array(count_array, (largest_num + 1));
+
+	/* Init. array to return sorted list */
+	sorted_array = malloc(sizeof(int) * size);
+
+	if (!sorted_array)
+	{
+		free(count_array);
+		return;
+	}
+
+	for (count1 = 0; count1 < size; count1++)
+	{
+		sorted_array[count_array[array[count1]] - 1] = array[count1];
+		count_array[array[count1]]--;
+	}
+	for (count1 = 0; count1 < size; count1++)
+	{
+		array[count1] = sorted_array[count1];
+	}
+	free(count_array);
+	free(sorted_array);
 }
