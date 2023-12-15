7f99fe6dcc1ff217b3096d33e29269fd32ca03cd
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-12-16 00:06:58 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,75 +1,93 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * quick_sort - perform quicksort algorithm
- * @array: array of integers
- * @size: size of the array
- * Return: void
+ * _swap - swaps two values in an array
+ *
+ * @array: data to sort
+ * @i: first value
+ * @j: second value
+ * @size: size of data
+ *
+ * Return: No Return
  */
-
-void quick_sort(int *array, size_t size)
+void _swap(int *array, int i, int j, int size)
 {
-	if (array == NULL || size < 2)
-		return;
-	quicksort_low_high(array, 0, size - 1, size);
-}
+	int tmp;
 
-/**
- * quicksort_low_high - function that includes low_idx and high_idx
- * @array: array of integers
- * @low_idx: low index in the array partitioned
- * @high_idx: high index in the array partitioned
- * @size: size of the original array
- * Return: void
- */
-
-void quicksort_low_high(int *array, int low_idx, int high_idx, size_t size)
-{
-	int pivot_idx;
-
-	if (low_idx < high_idx)
+	if (array[i] != array[j])
 	{
-		pivot_idx = Lotumo_partition(array, low_idx, high_idx, size);
-		quicksort_low_high(array, low_idx, pivot_idx - 1, size);
-		quicksort_low_high(array, pivot_idx + 1, high_idx, size);
-	}
-}
-
-/**
- * Lotumo_partition - perform Lotumo partition scheme
- * @array: array of integers
- * @low_idx: low index in the array partitioned
- * @high_idx: high index in the array partitioned
- * @size: size of the original array
- * Return: pivot index
- */
-
-int Lotumo_partition(int *array, int low_idx, int high_idx, size_t size)
-{
-	int pivot = array[high_idx];
-	int i = low_idx, j, temp;
-
-	for (j = i; j < high_idx; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			if (i != j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
-			i++;
-		}
-	}
-  
-	if (i != j)
-	{
-		temp = array[i];
-		array[i] = array[high_idx];
-		array[high_idx] = temp;
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
 		print_array(array, size);
 	}
+}
+
+/**
+ * partition - sorts a partition of data in relation to a pivot
+ *
+ * @array: data to sort
+ * @min: Left wall
+ * @max: right wall
+ * @size: size of data
+ *
+ * Return: New Pivot
+ */
+int partition(int *array, int min, int max, size_t size)
+{
+	int i = min, j, pivot  = array[max];
+
+	for (j = min; j <= max; j++)
+	{
+		if (array[j] < pivot)
+		{
+			_swap(array, i, j, size);
+			i++;
+		}
+
+	}
+	_swap(array, i, max, size);
+
 	return (i);
+}
+
+/**
+ * quicksort -  sorts an array of integers in ascending order using the
+ * Quick sort algorithm Lomuto partition scheme
+ *
+ * @array: data to sort
+ * @min: Left wall
+ * @max: right wall
+ * @size: size of data
+ *
+ * Return: No Return
+ */
+void quicksort(int *array, int min, int max, size_t size)
+{
+	int p;
+
+	if (min < max)
+	{
+		p = partition(array, min, max, size);
+		quicksort(array, min, p - 1, size);
+		quicksort(array, p + 1, max, size);
+	}
+}
+
+/**
+ * quick_sort -  sorts an array of integers in ascending order using the
+ * Quick sort algorithm Lomuto partition scheme
+ *
+ * @array: data to sort
+ * @size: size of data
+ *
+ * Return: No Return
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
 }
